a61b1583f098c0d5cd835dfc0ebcb4fa2403f729
Shkodi <sh40385@universum-ks.org>
2021-12-28 14:18:54 +0100

    Add project files.

---
+++ EventManagement/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManagement.Models
+{
+    public class Role
+    {
+        public long Id { get; set; }
+        public string Roles { get; set; }
+    }
+}
