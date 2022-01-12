acd22ef56b444c97ddd106a96ffcdbc864cbcbd0
Shkodi <77227480+Shkodi@users.noreply.github.com>
2022-01-12 11:16:21 +0100

    Add files via upload
    api commit

---
+++ EventManagementAPI/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManagementAPI.Models
+{
+    public class Event
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Campus { get; set; }
+        public DateTime Date { get; set; }
+        public string Room { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ EventManagementAPI/Models/Lecturer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManagementAPI.Models
+{
+    public class Lecturer
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+    }
+}
